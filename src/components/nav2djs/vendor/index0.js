6452c72b3527df23c4da1759fc878ac42d01e097
her_username <nuratabanjeh96@gmail.com>
2022-05-20 22:06:51 +0300

    initial

--- src/components/nav2djs/vendor/index0.js
+++ src/components/nav2djs/vendor/index0.js
@@ -79,7 +79,7 @@ var Nav2d = function (_Component) {
 
 Nav2d.defaultProps = {
   ros: new ROSLIB.Ros({
-    url: 'wss://3.141.4.113:9090'
+    url: 'ws://3.141.4.113:9090'
   }),
   id: 'nav2d',
   width: 800,
